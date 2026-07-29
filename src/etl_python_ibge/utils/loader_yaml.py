e66e25343cb6267210bdb441b607af1ee39a90fa
jcarlossc <jcarlossc1977@gmail.com>
2026-07-29 17:52:09 -0300

    update: Capturadores de erros adicionados em loader_yaml

--- src/etl_python_ibge/utils/loader_yaml.py
+++ src/etl_python_ibge/utils/loader_yaml.py
@@ -7,21 +7,24 @@ def load_all_configs(config_path: Path) -> Dict[str, Any]:
 
     configs: Dict[str, Any] = {}
 
-    if not config_path.exists():
-        raise FileNotFoundError(
-            f"Diretório de configuração não encontrado: {config_path}"
-        )
+    try:
+        if not config_path.exists():
+            raise FileNotFoundError(
+                f"Diretório de configuração não encontrado: {config_path}"
+            )
 
-    if not config_path.is_dir():
-        raise NotADirectoryError(
-            f"Caminho informado não é diretório: {config_path}"
-        )
+        if not config_path.is_dir():
+            raise NotADirectoryError(
+                f"Caminho informado não é diretório: {config_path}"
+            )
 
-    for file in config_path.glob("*.yaml"):
+        for file in config_path.glob("*.yaml"):
 
-        with open(file, "r", encoding="utf-8") as f:
+            with open(file, "r", encoding="utf-8") as f:
 
-            configs[file.stem] = yaml.safe_load(f)
+                configs[file.stem] = yaml.safe_load(f)
 
-    return configs
+        return configs
 
+    except yaml.YAMLError as error:
+        raise ValueError(f"YAML_ERROR: erro ao processar YAML -> {error}") from error
