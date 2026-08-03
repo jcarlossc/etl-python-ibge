feb3c9830591184baefbdfb6d64d60ca4571cc4b
jcarlossc <jcarlossc1977@gmail.com>
2026-08-03 15:23:11 -0300

    feat: Arquivo convert_to_df e pandas adicionados

---
+++ src/etl_python_ibge/converts/convert_to_df.py
@@ -0,0 +1,50 @@
+import logging
+from typing import Any
+
+import pandas as pd
+
+
+def get_convert(
+    data: list[list[dict[str, Any]]],
+) -> pd.DataFrame:
+    logger = logging.getLogger(__name__)
+
+    logger.info("Iniciando convrsão e limpeza dos dados.")
+
+    result = []
+
+    for response in data:
+        for indicator in response:
+            unit = indicator.get("unidade", {})
+
+            for serie in indicator.get("series", []):
+                country = serie.get("pais", {})
+
+                for records in serie.get("serie", []):
+                    for ano, valor in records.items():
+                        if not ano.isdigit():
+                            continue
+
+                        valor = pd.to_numeric(
+                            valor,
+                            errors="coerce",
+                        )
+
+                        if pd.notna(valor):
+                            result.append(
+                                {
+                                    "id_indicador": indicator.get("id"),
+                                    "indicador": indicator.get("indicador"),
+                                    "id_unidade": unit.get("id"),
+                                    "classe": unit.get("classe"),
+                                    "multiplicador": unit.get("multiplicador"),
+                                    "sigla_pais": country.get("id"),
+                                    "pais": country.get("nome"),
+                                    "ano": int(ano),
+                                    "valor": valor,
+                                }
+                            )
+
+    logger.info("Finalizando a convrsão e limpeza dos dados.")
+
+    return pd.DataFrame(result)
