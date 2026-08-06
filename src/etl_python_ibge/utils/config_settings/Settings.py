7a631779ccd5c1e1cbe6374c2ee898e3d1861158
jcarlossc <jcarlossc1977@gmail.com>
2026-08-06 15:04:43 -0300

    chore: Classe Settings configurada

---
+++ src/etl_python_ibge/utils/config_settings/Settings.py
@@ -0,0 +1,28 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    """
+    Representa as configurações da aplicação.
+
+    Todos os atributos são carregados automaticamente do
+    arquivo .env.
+    """
+
+    # Endereço do servidor MySQL.
+    mysql_host: str
+
+    # Porta utilizada pelo servidor MySQL.
+    mysql_port: int = 3306
+
+    # Usuário do banco.
+    mysql_user: str
+
+    # Senha do banco.
+    mysql_password: str
+
+    # Configuração do Pydantic.
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
