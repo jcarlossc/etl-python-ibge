e5322786c50e4c7ebc0e732729c41b97f7394dc5
jcarlossc <jcarlossc1977@gmail.com>
2026-08-07 18:31:49 -0300

    docs: Comentários e docstring adicionados

--- src/etl_python_ibge/sql/execute/execute_sql.py
+++ src/etl_python_ibge/sql/execute/execute_sql.py
@@ -10,18 +10,46 @@ def execute_sql(
     engine: Engine,
     sql_file: Path,
 ) -> None:
+    """
+    Executa um arquivo SQL.
+
+    A função lê um arquivo contendo comandos SQL e executa
+    cada instrução sequencialmente utilizando uma conexão
+    SQLAlchemy.
+
+    Parameters
+    ----------
+    engine : Engine
+        Engine utilizada para conexão com o banco de dados.
+
+    sql_file : Path
+        Caminho do arquivo SQL.
+
+    Raises
+    ------
+    FileNotFoundError
+        Caso o arquivo SQL não exista.
+
+    RuntimeError
+        Caso ocorra erro durante a execução do script SQL.
+    """
+
     logger = logging.getLogger(__name__)
 
     logger.info("Executando script SQL: %s", sql_file)
 
     try:
+        # Lê todo o conteúdo do arquivo SQL.
         sql = sql_file.read_text(encoding="utf-8")
 
+        # Separa os comandos SQL utilizando ';'.
         statements = [
             statement.strip() for statement in sql.split(";") if statement.strip()
         ]
 
+        # Abre uma transação.
         with engine.begin() as connection:
+            # Executa cada comando SQL.
             for statement in statements:
                 connection.execute(text(statement))
 
