b0f23ec1b4ea70c7fedc0f38f2f5e884db1b1cc2
jcarlossc <jcarlossc1977@gmail.com>
2026-08-08 15:01:29 -0300

    update: Capturadores de erros adicionados

--- src/etl_python_ibge/load/load_table.py
+++ src/etl_python_ibge/load/load_table.py
@@ -2,8 +2,7 @@ import logging
 
 import pandas as pd
 from sqlalchemy.engine import Engine
-
-# from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.exc import SQLAlchemyError
 
 
 def load_table(
@@ -18,17 +17,26 @@ def load_table(
         table_name,
     )
 
-    dataframe.to_sql(
-        name=table_name,
-        con=engine,
-        if_exists="append",
-        index=False,
-        chunksize=1000,
-        method="multi",
-    )
+    try:
+        dataframe.to_sql(
+            name=table_name,
+            con=engine,
+            if_exists="append",
+            index=False,
+            chunksize=1000,
+            method="multi",
+        )
 
-    logger.info(
-        "Tabela '%s' carregada com sucesso. Registros: %d.",
-        table_name,
-        len(dataframe),
-    )
+        logger.info(
+            "Tabela '%s' carregada com sucesso. Registros: %d.",
+            table_name,
+            len(dataframe),
+        )
+
+    except SQLAlchemyError as error:
+        logger.exception(
+            "Erro ao carregar a tabela '%s'.",
+            table_name,
+        )
+
+        raise RuntimeError(f"Erro ao carregar a tabela '{table_name}'.") from error
